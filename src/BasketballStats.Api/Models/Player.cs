0761519941e163b824dd7d44bb2ce1c75359707c
Robbie McMahen <mcmahenrobbie@gmail.com>
2026-04-28 22:16:28 +1000

    fix: resolved warnings for Name parameter in both Player and Team models

--- src/BasketballStats.Api/Models/Player.cs
+++ src/BasketballStats.Api/Models/Player.cs
@@ -1,7 +1,7 @@
 public class Player
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public int JerseyNumber { get; set; }
     public int TeamId { get; set; }
 }
--- src/BasketballStats.Api/Models/Team.cs
+++ src/BasketballStats.Api/Models/Team.cs
@@ -1,5 +1,5 @@
 public class Team
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
 }
